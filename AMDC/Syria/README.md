e6f7bf4475b7f50d61ee6f71b8c36a2307aecf6f
Mateusz Różyński <knifejjestem@gmail.com>
2026-01-05 23:57:00 +0100

    Made some README for AMDC Syria
    Still trying to fix CHIEF.

--- AMDC/Syria/README.md
+++ AMDC/Syria/README.md
@@ -1,3 +1,26 @@
-# Soon™
+# AMDC Syria Progress
 
-![](https://tenor.com/view/soon-tm-soon-space-gif-23401891.gif)
+### - Unit Spawning ✅
+〔 - Units spawn in defined zones randomly in amount defined in zones.lua
+〔 - SAM types vary by what difficulty of zone are they spawned in. These are Easy (SA-3, SA-6), Medium (SA-2, SA-11), Hard (SA-10, SA-5).
+〔 - Zones and airports have seperate spawning amounts.
+### - Capturable Zones ✅
+〔 - OPSZONES class is pretty simple zones have 3 states Guarded, Conflict and Empty. 
+〔 - Guarded means the zone is Guarded by forces that captured the zone and is not under attack. 
+〔 - Conflict means there are both Friendly and Enemy forces in the zone.
+〔 - Empty means that the zone is capturable by either Blue or Red forces.
+### - MANTIS Integration ✅
+〔 - Smarter SAM AI. Now SAM's can detect HARM's and react properly.
+〔 - SAM's will use the EWR network to theyre advantage. SAM's will not shoot until theyre active then they will activate and shoot at you.
+### - Redair GCICAP ✅
+〔 - Red forces are doing CAP and Intercepts based on the EWR network.
+〔 - Enemy aircraft will either do a racetrack pattern around theyre airfiled or a defined zone
+〔 - Enemy planes will also intercept incoming aircraft.
+### - Supply system 🔨
+〔 - Not yet decided on the solution here.
+### - CTLD Integration 🔨
+〔 - Deploying capture squads/airport defenses and transporting supply.
+### - CHIEF Integration 🔨
+〔 - Dynamic deploying of A2G, A2A, G2G units by both Red & Blue forces.
+### - Moving frontline/borders 🔨
+〔 - Pretty self-explanatory
--- AMDC/Syria/chief.lua
+++ AMDC/Syria/chief.lua
@@ -1,24 +1,34 @@
 Opszones = { Reneopszone, Beirutopszone, Basselopszone, Hatayopszone, Wujahopszone, Latakiaopszone }
 
-Platoon1 = PLATOON:New("capture_b_tank", 10, "M1A2 SEPv3 Abrams Platoon")
+Platoon1 = PLATOON:New("blue_ca_unit", 10, "M1A2 SEPv3 Abrams Platoon")
 Platoon1:AddMissionCapability({AUFTRAG.Type.GROUNDATTACK, AUFTRAG.Type.CAPTUREZONE}, 90)
-local warehouse = STATIC:FindByName("Warehouse Incirlik-1")
-if warehouse == nil then
-end
-env.info(warehouse:GetName())
+
+FS13 = SQUADRON:New("fa18c_squadron", 8, "F/A-18C Squadron")
+FS13:SetGrouping(2)                      -- Two-ships. Good to have a wingmen.
+FS13:SetModex(100)                       -- Onboard numbers are 100, 101, ...
+FS13:SetCallsign(CALLSIGN.Aircraft.Ford) -- Call sign is Ford.
+FS13:SetRadio(260)                       -- Squadon communicates on 260 MHz AM.
+FS13:SetSkill(AI.Skill.EXCELLENT)        -- These guy are really good.
+FS13:AddMissionCapability({AUFTRAG.Type.ORBIT, AUFTRAG.Type.INTERCEPT, AUFTRAG.Type.CAP, AUFTRAG.Type.ESCORT}, 90)
 
 BrigadeIncirlik = BRIGADE:New(STATIC:FindByName("Warehouse Incirlik-1"):GetName(), "Incirlik Brigade")
+AirwingIncirlik = AIRWING:New(STATIC:FindByName("Warehouse Incirlik-1"):GetName(), "Incirlik Airwing")
 
 if BrigadeIncirlik == nil then
 end
 BrigadeIncirlik:AddPlatoon(Platoon1)
+if AirwingIncirlik == nil then
+end
+AirwingIncirlik:AddSquadron(FS13)
 
 Agents = SET_GROUP:New():FilterPrefixes({"Blue EWR", "Blue", "Aerial"}):FilterStart()
 
 Chief = CHIEF:New(coalition.side.BLUE, Agents)
 
 Chief:SetTacticalOverviewOn()
+Chief:SetStrategy(CHIEF.Strategy.OFFENSIVE)
 Chief:AddBrigade(BrigadeIncirlik)
+Chief:AddAirwing(AirwingIncirlik)
 Chief:__Start(1)
 Chief:AddStrategicZone(Hatayopszone, 50, 1)
 function Chief:OnAfterNewContact(From, Event, To, Contact)
@@ -33,5 +43,10 @@ function Chief:OnAfterNewContact(From, Event, To, Contact)
         ContactThreat)
     -- Show message in log file.
     env.info(text)
+end
+
+function Chief:OnAfterMissionAssign(From, Event, To, Mission, Legions)
+    local text2 = string.format("Debug message" .. From  ..  Event  ..  To  ..  Mission  ..  Legions)
+    env.info(text2)
 end
 
